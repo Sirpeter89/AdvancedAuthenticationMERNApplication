d88156fb58b2f869321ac9a7abbef8f62aa77afc
Justin <just2rules@gmail.com>
2022-11-22 17:56:54 -0800

    added authorization middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken'
+import User from '../models/User.js'
+import ErrorResponse from '../utils/errorResponse'
+
+async function protect(req, res, next) {
+    let token
+
+    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        //example of what the token will look like:Bearer fnasfasfjlkjaslk
+        token = req.headers.authorization.split(' ')[1]
+    }
+
+    if (!token) {
+        return next(new ErrorResponse('Not authorized to access this route', 401))
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await User.findById(decoded.id)
+
+        if (!user) {
+            return next(new ErrorResponse('No user found with this id', 404))
+        }
+
+        req.user = user
+        next()
+    } catch (error) {
+        return next(new ErrorResponse('Not authorized to access this route', 401))
+    }
+}
+
+export default protect
